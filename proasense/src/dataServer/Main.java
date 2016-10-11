072c254a3dd24c74ae2d35a665b4526c866461ab
PedroBarrocaUni <pedro.barroca@uninonva.pt>
2016-10-11 01:04:11 +0100

    update3

--- proasense/src/dataServer/Main.java
+++ proasense/src/dataServer/Main.java
@@ -1163,14 +1163,14 @@ public class Main extends HttpServlet {
 
 	public void init() {
 		ServletContext context = getServletContext();
-		logPath = System.getProperty("home")
+		logPath = System.getProperty("user.home")
 				+File.separator+"proasense"
 				+File.separator+"proasenseModeller"
 				+File.separator+"KPIrepMaven"
 				+File.separator;
 		// Database that needs to be used IMPORTANT:has its identifiers as
 		// uppercase
-		dbPath = System.getProperty("home")
+		dbPath = System.getProperty("user.home")
 				+File.separator+"proasense"
 				+File.separator+"proasenseModeller"
 				+File.separator+"KPIrepMaven"
